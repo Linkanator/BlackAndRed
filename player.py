e81fda1a458d93df0dba6d4e85b6ed85593e1607
root <root@LAPTOP-MJJDPFOD.localdomain>
2019-02-16 00:40:00 -0500

    Create Player Class
    Create Player Class that holds its own checker pieces in a list. Provide methods to add to, remove from, and
    return the list of pieces.

--- player.py
+++ player.py
@@ -4,16 +4,25 @@ class Player:
     '''
 
     """Initializes a Player
-    @param pieces: a list of checker pieces that the player starts off with
+    @param Piece pieces: a list of checker pieces that the player starts off with
     """
     def __init__(self, pieces):
         self.pieces = pieces
 
+    """Adds a checker piece to the current list of owned pieces
+    @param Piece piece: checker piece to be added
+    """
     def add_piece(self, piece):
         self.pieces.append(piece)
 
+    """Removes a checker piece from the list of owned pieces
+    @param Piece piece: checker piece to be removed
+    """
     def kill_piece(self, piece):
         self.pieces.remove(piece)
 
+    """Return list of checker pieces currently owned
+    @return Array pieces: list of pieces
+    """
     def get_pieces(self):
         return self.pieces
