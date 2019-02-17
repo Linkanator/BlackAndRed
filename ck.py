f7b3f3ca080dec87ef907be9ddc6c57f77047063
Raaid <47687214+raaidsm@users.noreply.github.com>
2019-02-17 17:44:44 -0500

    Update ck.py

--- ck.py
+++ ck.py
@@ -11,8 +11,6 @@ class Checkers:
 class CheckersController:
     def __init__(self):
         """Initialize a new CheckersController object."""
-        position_from = input("Which piece to move?: ")
-        position_to = input("Where to move to?: ")
 
     def play():
         """Run the game until is_game_won."""
