b43b9b6bd3f95a19a45d5505f5de35aca7f5124c
root <root@LAPTOP-MJJDPFOD.localdomain>
2019-03-20 01:56:31 -0400

    Create Board Class
    Create Board class object that keeps track of which tiles are empty and which tiles are occupied by a player
    If a tile is occupied, it also keeps track of which player is currently occupying it

---
+++ game_board.py
@@ -0,0 +1,43 @@
+class Board:
+    '''A Board object that keeps track of which tiles are being occupied and by 
+    which player each tile is being occupied by'''
+    
+    def __init__(self):
+        """Initializes an empty 8x8 Board
+        """
+        self.board = []
+        for x in range(8):
+            self.board.append(["o", "o", "o", "o", "o", "o", "o", "o"])
+            
+    def is_tile_empty(self, x, y):
+        """Returns if the tile at the specified location is empty or occupied by 
+        a player
+        @param x horizontal index of the tile
+        @param y vertical index of the tile
+        @return a string indicating either an empty space or a space occupied by
+        player1 or player2
+        """
+        return self.board[x][y]
+        
+    def empty_tile(self, x, y):
+        """Empties the specified tile
+        @param x horizontal index of the tile
+        @param y vertical index of the tile
+        """        
+        self.board[x][y] = "o"
+        
+    def fill_tile(self, x, y, player):
+        """Occupy the spicified tile with the provided player
+        @param x horizontal index of the tile
+        @param y vertical index of the tile
+        @param player a string indicating if player1 or player2 are occupying
+        the tile
+        """        
+        self.board[x][y] = player
+        
+    def clear_board(self):
+        """Clear the game board
+        """        
+        for x in range(8):
+            for y in range(8):
+                self.board[x][y] = "o"
